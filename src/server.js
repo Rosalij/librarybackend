506a1241afd884ff4ddcdde5b26470c723440d21
Rosali <rosali.alfta@gmail.com>
2026-02-27 17:25:01 +0100

    corrected connection

--- src/server.js
+++ src/server.js
@@ -1,32 +1,33 @@
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 // Import routes
-const authRoutes = require("./src/routes/authRoutes");
-const reviewRoutes = require("./src/routes/reviewRoutes");
+const authRoutes = require("./routes/authRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
 
 const app = express();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/reviews", reviewRoutes);
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
 
 // Connect to MongoDB and start server
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
-  .catch(err => console.error(err));
-
-  app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
+  .catch(err => console.error("MongoDB connection error:", err));
