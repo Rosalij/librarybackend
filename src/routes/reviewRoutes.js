1991903b789e8b165be75ec87648be1886358cb4
Rosali <rosali.alfta@gmail.com>
2026-03-03 15:34:07 +0100

    added latest reviews route

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -8,9 +8,12 @@ const {
   deleteReview
 } = require("../controllers/reviewController");
 // Routes
-router.get("/:bookId", getReviewsByBook);
+router.get("/latest", getLatestReviews);
+ router.get("/:bookId", getReviewsByBook);
 router.post("/", authMiddleware, createReview);
 router.put("/:id", authMiddleware, updateReview);
 router.delete("/:id", authMiddleware, deleteReview);
-
+ 
 module.exports = router;
+
+
